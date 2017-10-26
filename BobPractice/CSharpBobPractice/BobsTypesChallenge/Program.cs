1e3aa5146420c39565ed2076d92c5b0bc4f0ffe5
Carr OConnor <carr.oconnor@gmail.com>
2017-10-25 23:56:16 -0400

    Bobs Practice

---
+++ BobPractice/CSharpBobPractice/BobsTypesChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobsTypesChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 25;
+            int number2 = 50;
+
+            string myName = "Carr";
+            string lastName = "OConnor";
+
+            bool myTruth = true;
+
+            float myFloat = 3.14f;
+            float yourFloat = 8.33f;
+
+            double myDouble = 5.55;
+            double yourDouble = 6.55;
+
+            decimal myDecimal = 5.323423M;
+            decimal yourDecimal = 4.232342M;
+
+
+            //SILVER
+
+            string fullName = "Carr" + "OConnor";
+            Console.WriteLine(fullName);
+
+
+            //GOLD
+            string birthYear = "1980";
+            int year = Convert.ToInt32(birthYear);
+
+            string age = "30";
+
+            int numAge = Convert.ToInt32(age);
+            numAge = numAge + 1;
+
+            Console.WriteLine(numAge);
+            Console.ReadLine();
+
+        }
+    }
+}
